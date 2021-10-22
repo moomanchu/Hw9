59fa4b786b9d53fb26db1df23d6a76ed04260334
moomanchu <92184961+moomanchu@users.noreply.github.com>
2021-10-22 14:08:14 -0700

    Update starter.java

--- universal base code/starter.java
+++ universal base code/starter.java
@@ -8,6 +8,7 @@ public class starter implements InputControl, InputKeyControl {
 		for(int n = 0; n<ai.length; n++){
 			System.out.print(ai[n]+" ");
 		}
+		System.out.println();
 	}
 	public static void main(String args[]) {
 		// please leave following line alone, necessary for keyboard/mouse input
@@ -15,26 +16,26 @@ public class starter implements InputControl, InputKeyControl {
 		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
 		
 		int[] da = new int[7];
-		int max = 0;
+		//int max = 0;
 		for(int n=0; n<da.length; n++){
 			da[n] = Canvas.rand(10);
 			System.out.print(da[n]+" ");
-			if(da[n]>max){
-				max = da[n];
-			}
 		}
 		System.out.println();
-		System.out.println(max);
 		
-		int[] ta = new int[max+2];
+		int[] ta = new int[10];
 		printar(ta);
 		
-		for(int n=0;n<da.length;n++){
-			ta[da[n]] = 1;
+		int count;
+		for(int a=0;a<ta.length;a++){//constant on each number
+			for(int b=0;b<da.length;b++){//scans array for same number //checks how many times that number appears
+				if(a == da[b]){
+					ta[a]++;
+				}
+			}
 		}
-		System.out.println();
-		printar(ta);//now make it so it counts the amount of each data value appears
-		//nested for loop in for loop idea--> could work
+		printar(ta);
+		
 		
 		//what to do:
 		//data array: 2 5 8 7 4 3 2
